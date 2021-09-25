0941b0254828a2466ea698ce85d8b6be49bda442
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-09-25 20:15:48 +0900

    TypeScript practice_Number

---
+++ type-practice/number.ts
@@ -0,0 +1,16 @@
+// 10진수
+let decimal: number = 6;
+
+// 16진수
+let hex: number = 0xf00d;
+
+// 2진수
+let binary: number = 0b1010;
+
+// 8진수
+let octal: number = 0o744;
+
+let notANumber: number = NaN;
+
+// 100만
+let underscoreNum: number = 1_000_000;
