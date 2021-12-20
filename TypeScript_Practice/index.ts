85ffa3162db4d46a7f470cba72a1397d5d52aeb9
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-12-20 16:34:33 +0900

    Feat: void, ? 타입 연습

--- TypeScript_Practice/index.ts
+++ TypeScript_Practice/index.ts
@@ -7,8 +7,21 @@ let members: (number | string)[] = [1, "2", 3];
 // 실드 해제, 타입 관련 버그가 생길 경우 추적할 수 없다. 대신 어떤 타입이든 다 사용할 수 있다.
 let Hello: any = "Bye";
 
-//unknown
+// unknown
 // any와 동일하게 모든 타입을 집어넣을 수 있다.
 // 자료를 넣어도 타입은 그대로 unknown 이다.
 
 let friend: unknown;
+
+// void
+// 함수가 실수로 뭔가 return하는 것을 사전에 막을 수 있다.
+
+// ? 타입
+// 파라미터가 옵션인 경우
+// 변수? : number = 변수 : number | undefined
+
+function sum(x?: number): void {
+  1 + 1;
+}
+
+sum();
