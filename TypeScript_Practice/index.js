7068a7ff68ffd2b1cf95af7be6bb1f65a501ad31
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-12-20 14:50:36 +0900

    Feat: 타입스크립트 Primitive Types 간단 연습

---
+++ TypeScript_Practice/index.js
@@ -0,0 +1,9 @@
+var humanName = "kim";
+var age = 50;
+var isMarried = true;
+var member = ["kim", "park"];
+var members = {
+    member1: "kim",
+    member2: "park",
+};
+var human = "park";
---
+++ TypeScript_Practice/index.ts
@@ -0,0 +1,9 @@
+let humanName: string = "kim";
+let age: number = 50;
+let isMarried: boolean = true;
+let member: string[] = ["kim", "park"];
+let members: { member1: string; member2: string } = {
+  member1: "kim",
+  member2: "park",
+};
+let human = "park";
