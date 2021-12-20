810258c295fe288f9be0ce33c740fa95f13f8f63
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-12-20 16:18:47 +0900

    Feat: Union type, Any, Unknown

--- TypeScript_Practice/index.js
+++ TypeScript_Practice/index.js
@@ -1,9 +1,3 @@
-var humanName = "kim";
-var age = 50;
-var isMarried = true;
-var member = ["kim", "park"];
-var members = {
-    member1: "kim",
-    member2: "park",
-};
-var human = "park";
+// union type
+var member = 123;
+var members = [1, "2", 3];
--- TypeScript_Practice/index.ts
+++ TypeScript_Practice/index.ts
@@ -1,9 +1,14 @@
-let humanName: string = "kim";
-let age: number = 50;
-let isMarried: boolean = true;
-let member: string[] = ["kim", "park"];
-let members: { member1: string; member2: string } = {
-  member1: "kim",
-  member2: "park",
-};
-let human = "park";
+// Union type
+// 새로운 타입
+let member: number | string = 123;
+let members: (number | string)[] = [1, "2", 3];
+
+// any
+// 실드 해제, 타입 관련 버그가 생길 경우 추적할 수 없다. 대신 어떤 타입이든 다 사용할 수 있다.
+let Hello: any = "Bye";
+
+//unknown
+// any와 동일하게 모든 타입을 집어넣을 수 있다.
+// 자료를 넣어도 타입은 그대로 unknown 이다.
+
+let friend: unknown;
